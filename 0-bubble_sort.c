cccc31514839b41af4b6a1a40bd96a33b841977a
dina3538 <dinaaltayeb1234@gmail.com>
2024-01-18 05:28:48 -0800

    a

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function that sorts an array of integers in ascending order
+ * @array: array
+ * @size: the size
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	int i, m;
+
+	if (array == NULL || size == NULL)
+		return;
+
+	i = 0;
+	while (i < size)
+	{
+		for (m = 0; m < size - 1; m++)
+		{
+			if array[m] > array[m + 1]
+			{
+				temp = array[m];
+				array[m] = array[m + 1];
+				array[m + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
+}
