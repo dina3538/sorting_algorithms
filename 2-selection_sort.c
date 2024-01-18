27f8774dc9fde0698a1f542cae9a949d928a48c5
dina3538 <dinaaltayeb1234@gmail.com>
2024-01-18 13:30:47 -0800

    a

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,7 +15,7 @@ void selection_sort(int *array, size_t size)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		for (m = size - 1; d = i + 1; m > i; m--)
+		for (m = size - 1, d = i + 1; m > i; m--)
 		{
 			if (array[m] < array[d])
 			{
