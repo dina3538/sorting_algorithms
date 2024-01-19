c924837a78fb8d5311952297606783318dee693f
dina3538 <dinaaltayeb1234@gmail.com>
2024-01-19 09:13:38 -0800

    a

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -4,38 +4,38 @@
 /**
  * shell_sort - Sorts an array of integers in ascending order using Shell sort
  * @array: The array to be sorted
- * @size: Number of elements in @array
+ * @size: Number of elems
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t interval;
+	size_t k;
 	size_t i;
-	
+
 	if (array == NULL || size < 2)
 		return;
 
-	interval = 1;
+	k = 1;
 
-	while (interval < size / 3)
-		interval = interval * 3 + 1;
+	while (k < size / 3)
+		k = k * 3 + 1;
 
-	while (interval > 0)
+	while (k > 0)
 	{
-		for (i = interval; i < size; ++i)
+		for (i = k; i < size; ++i)
 		{
 			int temp = array[i];
 			size_t j;
 
-			for (j = i; j >= interval && array[j - interval] > temp; j -= interval)
+			for (j = i; j >= k && array[j - k] > temp; j -= k)
 			{
-				array[j] = array[j - interval];
+				array[j] = array[j - k];
 			}
 
 			array[j] = temp;
 		}
 
 		print_array(array, size);
-		interval = (interval - 1) / 3;
+		k = (k - 1) / 3;
 	}
 }
 
