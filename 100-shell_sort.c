aac6170c1b91b5be55cd1eb0ba5cc5270fbdfc43
dina3538 <dinaaltayeb1234@gmail.com>
2024-01-19 09:08:07 -0800

    a

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,17 +8,20 @@
  */
 void shell_sort(int *array, size_t size)
 {
+	size_t interval;
+	size_t i;
+	
 	if (array == NULL || size < 2)
 		return;
 
-	size_t interval = 1;
+	interval = 1;
 
 	while (interval < size / 3)
 		interval = interval * 3 + 1;
 
 	while (interval > 0)
 	{
-		for (size_t i = interval; i < size; ++i)
+		for (i = interval; i < size; ++i)
 		{
 			int temp = array[i];
 			size_t j;
@@ -32,7 +35,7 @@ void shell_sort(int *array, size_t size)
 		}
 
 		print_array(array, size);
-		interval = (interval - 1) / 3; // Decrease interval following Knuth sequence
+		interval = (interval - 1) / 3;
 	}
 }
 
