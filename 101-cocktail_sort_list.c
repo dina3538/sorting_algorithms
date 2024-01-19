81ef0ac5991a4751dce83b80d21419873d960e98
dina3538 <dinaaltayeb1234@gmail.com>
2024-01-19 09:41:07 -0800

    a

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -2,6 +2,29 @@
 #include <stdio.h>
 
 /**
+ * swap_nodes - Swaps two nodes in a doubly linked list
+ * @list: Pointer to the head of the doubly linked list
+ * @a: First node to swap
+ * @b: Second node to swap
+ */
+void swap_nodes(listint_t **list, listint_t **a, listint_t **b)
+{
+	listint_t *temp;
+	if (*a == NULL || *b == NULL)
+		return;
+	temp = (*a)->prev;
+	if (temp != NULL)
+		temp->next = *b;
+	else
+		*list = *b;
+	(*b)->prev = temp;
+	(*a)->prev = *b;
+	(*a)->next = (*b)->next;
+	if ((*b)->next != NULL)
+		(*b)->next->prev = *a;
+	(*b)->next = *a;
+}
+/**
  * cocktail_sort_list - Sorts a doubly linked list of integers
  * in ascending order using Cocktail Shaker sort
  * @list: Pointer to the head of the doubly linked list
@@ -43,33 +66,3 @@ void cocktail_sort_list(listint_t **list)
 
 	} while (swapped);
 }
-
-/**
- * swap_nodes - Swaps two nodes in a doubly linked list
- * @list: Pointer to the head of the doubly linked list
- * @a: First node to swap
- * @b: Second node to swap
- */
-void swap_nodes(listint_t **list, listint_t **a, listint_t **b)
-{
-	listint_t *temp;
-
-	if (*a == NULL || *b == NULL)
-		return;
-
-	temp = (*a)->prev;
-	if (temp != NULL)
-		temp->next = *b;
-	else
-		*list = *b;
-
-	(*b)->prev = temp;
-	(*a)->prev = *b;
-	(*a)->next = (*b)->next;
-
-	if ((*b)->next != NULL)
-		(*b)->next->prev = *a;
-
-	(*b)->next = *a;
-}
-
